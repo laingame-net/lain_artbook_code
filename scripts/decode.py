cd3a464f02a4b562242b4b370a04b02e2a0e3a01
vh <vh@smena.space>
2020-11-30 21:48:19 +0500

    An attempt to decode innocent.jpg

---
+++ scripts/decode.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import binhex_patched
+
+
+def decode(filename_in, filename_out):
+    binhex_patched.hexbin(filename_in, filename_out)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    p = argparse.ArgumentParser()
+    p.add_argument("in_file")
+    p.add_argument("out_file")
+    args = p.parse_args()
+
+    decode(args.in_file, args.out_file)
+
